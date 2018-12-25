bd8e29ecfc06d81edef0dcf060a26fd309f0858b
Shine <abhijit.shine@hotmail.com>
2018-12-25 17:23:01 -0500

    Updates

---
+++ precache-manifest.63fd11c4f9b0b52d194c0a052d652d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "80b3158ed49604275d72",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/static/js/runtime~main.80b3158e.js"
+  },
+  {
+    "revision": "e87022e213d256f6e7c6",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/static/js/main.e87022e2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e87022e213d256f6e7c6",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/static/css/main.efe3d9f9.chunk.css"
+  },
+  {
+    "revision": "2169d362f90084f583b3c19b641c393a",
+    "url": "shinesingh1994.github.io/shinesingh1994.github.io/index.html"
+  }
+];
